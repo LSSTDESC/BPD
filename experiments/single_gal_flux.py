8458e7f774eaee298efee58a3d3ea59a52ceff90
ismael2395 <imendoza@umich.edu>
2023-10-20 04:33:43 -0700

    better configuration

--- experiments/single_gal_flux.py
+++ experiments/single_gal_flux.py
@@ -1,37 +1,57 @@
 #!/usr/bin/env python
 """Run inference on a single galaxy with flux as a parameter."""
 import math
+import os
+from pathlib import Path
 
-import click
 import galsim as _galsim
 import jax
 import jax_galsim as galsim
 import numpy as np
 import numpyro
 import numpyro.distributions as dist
+import yaml
 from numpyro.infer import NUTS
 
 from bpd.chains import run_chains
 from bpd.draw import add_noise
 from bpd.save_load import save_samples
 
-PSF_HLR = 0.7
-PIXEL_SCALE = 0.2
-SLEN = 101
-BACKGROUND = 1e4
-NOISE = 1
-
-# galaxy
-HLR = 0.9
-TRUE_FLUX = 1e5
-TRUE_LOG_FLUX = np.log10(TRUE_FLUX)
-
 GSPARAMS = galsim.GSParams(minimum_fft_size=512, maximum_fft_size=512)
+
+# read yaml config
+config_filename = "single_gal_flux.yaml"
+cwd = Path(os.path.dirname(os.path.abspath(__file__)))
+with open(cwd / "configs" / config_filename, "r") as f:
+    config = yaml.load(f, Loader=yaml.FullLoader)
+
+# set up image parameters
+SLEN = config["image"]["slen"]
+PIXEL_SCALE = config["image"]["pixel_scale"]
+BACKGROUND = config["image"]["background"]
+NOISE_FACTOR = config["image"]["noise_factor"]
+PSF_HLR = config["psf"]["psf_hlr"]
+
+# true galaxy parameters
+HLR = config["galaxy"]["hlr"]
+FLUX = config["galaxy"]["flux"]
+
+# chains parameters
+N_CHAIN = config["chains"]["n_chain"]
+N_VEC = config["chains"]["n_vec"]
+N_WARMUP = config["chains"]["n_warmup"]
+N_SAMPLES = config["chains"]["n_samples"]
+
+# gpu and seed
+GPU = config["gpu"]
+SEED = config["seed"]
+ID = config["run_id"]
 
 
 # get true image
 def _draw_gal():
-    gal = _galsim.Gaussian(flux=TRUE_FLUX, half_light_radius=HLR)
+    """Draw the target truth galaxy image."""
+    gal = _galsim.Gaussian(flux=FLUX, half_light_radius=HLR)
     psf = _galsim.Gaussian(flux=1.0, half_light_radius=PSF_HLR)
     gal_conv = _galsim.Convolve([gal, psf])
     gal_conv = gal_conv.drawImage(nx=SLEN, ny=SLEN, scale=PIXEL_SCALE, bandpass=None)
@@ -57,34 +77,27 @@ def prob_model(data: np.ndarray):
     numpyro.sample("obs", dist.Normal(im, math.sqrt(BACKGROUND)), obs=data)
 
 
-@click.command()
-@click.option("-g", "--gpu", default=0, type=int)
-@click.option("-n", "--n-chain", default=100, type=int)
-@click.option("-nv", "--n-vec", default=10, type=int)
-@click.option("-nw", "--n_warmup", default=100, type=int)
-@click.option("-ns", "--n_samples", default=1000, type=int)
-@click.option("-s", "--seed", default=42, type=int)
-def main(gpu: int, n_chain: int, n_vec: int, n_warmup: int, n_samples: int, seed: int):
+def main():
     # setup gpu
-    assert gpu in [0, 1, 2, 3]
-    print(f"Using GPU {gpu}")
-    jax.config.update("jax_default_device", jax.devices()[gpu])
+    assert GPU in [0, 1, 2, 3]
+    print(f"Using GPU {GPU}")
+    jax.config.update("jax_default_device", jax.devices()[GPU])
 
     nuts_kernel = NUTS(prob_model)
 
     true_image = _draw_gal()
-    data, _ = add_noise(true_image, n=n_vec * n_chain)
+    data, _ = add_noise(true_image, n=N_VEC * N_CHAIN)
 
     samples = run_chains(
         data,
         nuts_kernel,
-        n_vec=n_vec,
-        n_warmup=n_warmup,
-        n_samples=n_samples,
-        seed=seed,
+        n_vec=N_VEC,
+        n_warmup=N_WARMUP,
+        n_samples=N_SAMPLES,
+        seed=SEED,
     )
 
-    save_samples(samples, "single_gal_flux.hdf5")
+    save_samples(samples, "single_gal_flux.hdf5", group=f"run_{ID}")
 
 
 if __name__ == "__main__":
