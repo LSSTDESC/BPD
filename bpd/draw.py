ff221414a1f4a00f893fd449857185d109ae639c
ismael2395 <imendoza@umich.edu>
2023-10-20 08:07:46 -0700

    avoid bg missing errors which have been frequent

--- bpd/draw.py
+++ bpd/draw.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def add_noise(x: np.ndarray, n=1, noise_factor=1, bg=0.0):
+def add_noise(x: np.ndarray, bg: float, n=1, noise_factor=1):
     """Produce `n` independent Gaussian noise realizations of a given image `x`."""
     x = x.reshape(1, *x.shape)
     x = x.repeat(n, axis=0)
