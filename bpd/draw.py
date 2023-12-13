6758036eedaa5fa715f53bde64f1d17de19a7c45
ismael2395 <imendoza@umich.edu>
2023-12-13 12:02:35 -0800

    more specificity

--- bpd/draw.py
+++ bpd/draw.py
@@ -2,7 +2,10 @@ import numpy as np
 
 
 def add_noise(x: np.ndarray, bg: float, n=1, noise_factor=1):
-    """Produce `n` independent Gaussian noise realizations of a given image `x`."""
+    """Produce `n` independent Gaussian noise realizations of a given image `x`.
+
+    NOTE: This function assumes image is background-subtracted and dominated.
+    """
     x = x.reshape(1, *x.shape)
     x = x.repeat(n, axis=0)
     noise = np.random.randn(*x.shape) * np.sqrt(bg) * noise_factor
