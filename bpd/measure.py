5665d8653ca53727aecae7d462199dc4f9b5832b
ismael2395 <imendoza@umich.edu>
2024-08-19 14:08:15 -0700

    wrong assertion

--- bpd/measure.py
+++ bpd/measure.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 
-def get_snr(im, background: float) -> float:
+def get_snr(im: np.ndarray, background: float) -> float:
     """Calculate the signal-to-noise ratio of an image.
 
     Args:
         im: Image array with no background.
         background: Background level.
     """
-    assert im.shape == 2
+    assert im.ndim == 2
     assert isinstance(background, float) or background.shape == ()
     return np.sqrt(np.sum(im * im / (background + im)))
