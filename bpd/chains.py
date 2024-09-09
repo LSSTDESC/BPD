d39936a00f61fee41cd59330e4fe7034308001fb
ismael2395 <imendoza@umich.edu>
2024-09-09 13:29:39 -0700

    light refactoring of codebase given new developments in notebooks

--- bpd/chains.py
+++ bpd/chains.py
@@ -1,63 +1,14 @@
-from functools import partial
-
-import blackjax
 import jax
-from jax import jit as jjit
 
 
-def inference_loop(rng_key, kernel, initial_state, n_samples):
+def inference_loop(rng_key, initial_state, kernel, n_samples: int):
+    """Function to run a single chain with a given kernel and obtain `n_samples`."""
 
-    @jjit
     def one_step(state, rng_key):
         state, info = kernel(rng_key, state)
         return state, (state, info)
 
     keys = jax.random.split(rng_key, n_samples)
     _, (states, infos) = jax.lax.scan(one_step, initial_state, keys)
-
-    return (states, infos)
-
-
-def inference_loop_multiple_chains(
-    rng_key, kernel, initial_states, n_samples, n_chains
-):
-
-    @jjit
-    def one_step(states, rng_key):
-        keys = jax.random.split(rng_key, n_chains)
-        states, infos = jax.vmap(kernel)(keys, states)
-        return states, (states, infos)
-
-    keys = jax.random.split(rng_key, n_samples)
-    _, (states, infos) = jax.lax.scan(one_step, initial_states, keys)
-
-    return states, infos
-
-
-def inference_loop_multiple_chains_with_data(
-    rng_key,
-    initial_states,
-    tuned_params,
-    all_data,
-    kernel,
-    log_prob_fn,
-    n_samples,
-    n_chains,
-):
-    """Source: https://blackjax-devs.github.io/sampling-book/models/change_of_variable_hmc.html"""
-    # e.g.  kernel = blackjax.nuts.build_kernel()
-
-    @jjit
-    def step_fn(key, state, data, **params):
-        logdensity = partial(log_prob_fn, data=data)
-        return kernel(key, state, logdensity, **params)
-
-    def one_step(states, rng_key):
-        keys = jax.random.split(rng_key, n_chains)
-        states, infos = jax.vmap(step_fn)(keys, states, all_data, **tuned_params)
-        return states, (states, infos)
-
-    keys = jax.random.split(rng_key, n_samples)
-    _, (states, infos) = jax.lax.scan(one_step, initial_states, keys)
 
     return (states, infos)
--- bpd/draw.py
+++ bpd/draw.py
@@ -5,8 +5,7 @@ def add_noise(
     x: np.ndarray,
     bg: float,
     rng: np.random.Generator = np.random.default_rng(42),
-    n=1,
-    noise_factor=1,
+    n: int = 1,
 ):
     """Produce `n` independent Gaussian noise realizations of a given image `x`.
 
@@ -15,5 +14,5 @@ def add_noise(
     assert isinstance(bg, float) or bg.shape == ()
     x = x.reshape(1, *x.shape)
     x = x.repeat(n, axis=0)
-    noise = rng.normal(loc=0, scale=np.sqrt(bg), size=x.shape) * noise_factor
+    noise = rng.normal(loc=0, scale=np.sqrt(bg), size=x.shape)
     return x + noise
