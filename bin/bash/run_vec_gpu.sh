5cbcd2c2d543a79d85113748484b65ca28eccb74
ismael2395 <imendoza@umich.edu>
2024-10-07 15:35:59 -0700

    no changes

--- bin/bash/run_vec_gpu.sh
+++ bin/bash/run_vec_gpu.sh
@@ -2,4 +2,4 @@ export CUDA_VISIBLE_DEVICES="1"
 echo "Using CUDA ${CUDA_VISIBLE_DEVICES}"
 export JAX_ENABLE_X64="False"
 
-./vect_toy_shear_gpu.py --n-samples-gals 1000 --n-samples-shear 3000 --n-vec 100 --start-seed 1 --end-seed 1000 --tag "gpu3_vec" --k 1000 --trim 2
+./vect_toy_shear_gpu.py --n-samples-gals 1000 --n-samples-shear 3000 --n-vec 100 --start-seed 1 --end-seed 1000 --tag "gpu3_vec" --k 200 --trim 1
