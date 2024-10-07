4f691fcb7cdb3da23828d894fd3dbea2c9a5d848
ismael2395 <imendoza@umich.edu>
2024-10-07 14:40:40 -0700

    trying different trimmings

--- bin/run_vec_gpu.sh
+++ bin/run_vec_gpu.sh
@@ -1,5 +1,5 @@
-export CUDA_VISIBLE_DEVICES="0"
+export CUDA_VISIBLE_DEVICES="1"
 echo "Using CUDA ${CUDA_VISIBLE_DEVICES}"
 export JAX_ENABLE_X64="False"
 
-./vect_toy_shear_gpu.py --n-samples-gals 1000 --n-samples-shear 3000 --n-vec 100 --start-seed 1 --end-seed 1000 --tag "gpu2_vec" --k 1000 --trim 10
+./vect_toy_shear_gpu.py --n-samples-gals 1000 --n-samples-shear 3000 --n-vec 100 --start-seed 1 --end-seed 1000 --tag "gpu3_vec" --k 1000 --trim 2
