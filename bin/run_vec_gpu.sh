dc00869f3a882eb12fdc35472ea33129dd5885cb
ismael2395 <imendoza@umich.edu>
2024-10-07 14:20:35 -0700

    run vectorized pipeline

---
+++ bin/run_vec_gpu.sh
@@ -0,0 +1,5 @@
+export CUDA_VISIBLE_DEVICES="0"
+echo "Using CUDA ${CUDA_VISIBLE_DEVICES}"
+export JAX_ENABLE_X64="False"
+
+./vect_toy_shear_gpu.py --n-samples-gals 1000 --n-samples-shear 3000 --n-vec 100 --start-seed 1 --end-seed 1000 --tag "gpu2_vec" --k 1000 --trim 10
