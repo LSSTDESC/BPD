21016434f7b874f745a3151f3ac8f62f4d63e891
ismael2395 <imendoza@umich.edu>
2024-10-07 08:39:05 -0700

    add more options to running gpu job

--- bin/run_toy_gpu.sh
+++ bin/run_toy_gpu.sh
@@ -1,7 +1,12 @@
 
-export CUDA_VISIBLE_DEVICES="0"
+export CUDA_VISIBLE_DEVICES=$1
+echo "Using CUDA ${CUDA_VISIBLE_DEVICES}"
+export JAX_ENABLE_X64="False"
+START=$2
+END=$3
 
-
-# ./get_toy_ellip_samples.py --n-samples 10_000 --seed 39 --tag cpu_test1 --k 100 --obs-noise 1e-4 --shape-noise 1e-3 --> EASY dont need GPU
-
-./get_shear_from_post_ellips.py --seed 401 --e-samples-file /pscratch/sd/i/imendoza/data/cache_chains/e_post_42_cpu_test2.npz
+for ((i = $START; i <= $END; i++)); do
+    echo $i
+    ./get_toy_ellip_samples.py --tag gpu2 --seed $i --k 50
+    ./get_shear_from_post_ellips.py --tag gpu2 --seed $i --e-samples-fname e_post_${i}.npz
+done
