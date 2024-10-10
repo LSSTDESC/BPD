b661000b5059d5c65273f12320e07c599e00e8b0
ismael2395 <imendoza@umich.edu>
2024-10-10 09:09:15 -0700

    slightly more CLI input

--- bin/bash/run_vec_gpu.sh
+++ bin/bash/run_vec_gpu.sh
@@ -1,4 +1,6 @@
-export CUDA_VISIBLE_DEVICES="1"
+export CUDA_VISIBLE_DEVICES=$1
 echo "Using CUDA ${CUDA_VISIBLE_DEVICES}"
+START=$2
+END=$3
 
-./vect_toy_shear_gpu.py --n-samples-gals 1000 --n-samples-shear 3000 --n-vec 100 --start-seed 1 --end-seed 1000 --tag "gpu3_vec" --k 200 --trim 1
+./vect_toy_shear_gpu.py --n-samples-gals 1000 --n-samples-shear 3000 --n-vec 100 --start-seed $START --end-seed $END --tag "gpu1_vec" --k 100 --trim 1
