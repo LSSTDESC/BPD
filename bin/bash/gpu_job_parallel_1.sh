165d19f92eac658d1826372514134bad512429b5
ismael2395 <imendoza@umich.edu>
2024-10-10 13:12:57 -0700

    jobs

---
+++ bin/bash/gpu_job_parallel_1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --account=m1727
+#SBATCH -C gpu
+#SBATCH -N 1
+#SBATCH -t 00:20:00
+#SBATCH --ntasks-per-node=4
+#SBATCH --mail-type=begin,end,fail
+#SBATCH --mail-user=imendoza@umich.edu
+
+#ref: https://docs.nersc.gov/systems/perlmutter/running-jobs/#single-gpu-tasks-in-parallel
+K=1000
+TRIM=10
+TAG="gpu1_vec_n100"
+
+srun --exact -u -n 1 --gpus-per-task 1 -c 1 --mem-per-cpu=40G ./vect_toy_shear_gpu.py --n-samples-gals 1000 --n-samples-shear 3000 --n-vec 100 --start-seed 1 --end-seed 250 --tag $TAG --k $K --trim $TRIM &
+srun --exact -u -n 1 --gpus-per-task 1 -c 1 --mem-per-cpu=40G ./vect_toy_shear_gpu.py --n-samples-gals 1000 --n-samples-shear 3000 --n-vec 100 --start-seed 251 --end-seed 500 --tag $TAG --k $K --trim $TRIM &
+srun --exact -u -n 1 --gpus-per-task 1 -c 1 --mem-per-cpu=40G ./vect_toy_shear_gpu.py --n-samples-gals 1000 --n-samples-shear 3000 --n-vec 100 --start-seed 501 --end-seed 750 --tag $TAG --k $K --trim $TRIM &
+srun --exact -u -n 1 --gpus-per-task 1 -c 1 --mem-per-cpu=40G ./vect_toy_shear_gpu.py --n-samples-gals 1000 --n-samples-shear 3000 --n-vec 100 --start-seed 751 --end-seed 1000 --tag $TAG --k $K --trim $TRIM &
+
+wait
--- bin/bash/run_vec_gpu.sh
+++ bin/bash/run_vec_gpu.sh
@@ -3,4 +3,4 @@ echo "Using CUDA ${CUDA_VISIBLE_DEVICES}"
 START=$2
 END=$3
 
-./vect_toy_shear_gpu.py --n-samples-gals 1000 --n-samples-shear 3000 --n-vec 100 --start-seed $START --end-seed $END --tag "gpu1_vec" --k 100 --trim 1
+./vect_toy_shear_gpu.py --n-samples-gals 1_000 --n-samples-shear 3000 --n-vec 100 --start-seed $START --end-seed $END --tag "gpu3_vec" --k 100 --trim 1
