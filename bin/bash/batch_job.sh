3f588fe8b9fc6acb9c935e82771c559fc635f9ae
ismael2395 <imendoza@umich.edu>
2024-10-13 15:09:11 -0700

    remove .sh files

--- bin/bash/batch_job.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --account=m1727
-#SBATCH --nodes=1
-#SBATCH --time=00:20:00
-#SBATCH --constraint=cpu
-
-conda init
-conda activate bpd_cpu2
-srun ./to_run.sh
--- bin/bash/run_vec_gpu.sh
+++
@@ -1,6 +0,0 @@
-export CUDA_VISIBLE_DEVICES=$1
-echo "Using CUDA ${CUDA_VISIBLE_DEVICES}"
-START=$2
-END=$3
-
-./vect_toy_shear_gpu.py --n-samples-gals 1_000 --n-samples-shear 3000 --n-vec 100 --start-seed $START --end-seed $END --tag "gpu3_vec" --k 100 --trim 1
