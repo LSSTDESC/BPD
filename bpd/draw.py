4ced761efcdc7d31b5159c547d530a340c26944c
ismael2395 <imendoza@umich.edu>
2024-10-22 14:57:32 -0700

    add galsim drawing too

--- bpd/draw.py
+++ bpd/draw.py
@@ -1,3 +1,4 @@
+import galsim
 import jax_galsim as xgalsim
 
 GSPARAMS = xgalsim.GSParams(minimum_fft_size=256, maximum_fft_size=256)
@@ -23,5 +24,28 @@ def draw_gaussian(
 
     psf = xgalsim.Gaussian(flux=1, half_light_radius=psf_hlr)
     gal_conv = xgalsim.Convolve([gal, psf]).withGSParams(GSPARAMS)
+    image = gal_conv.drawImage(nx=slen, ny=slen, scale=pixel_scale, offset=(x, y))
+    return image.array
+
+
+def draw_gaussian_galsim(
+    f: float,
+    hlr: float,
+    e1: float,
+    e2: float,
+    g1: float,
+    g2: float,
+    x: float,
+    y: float,
+    pixel_scale: float = 0.2,
+    slen: int = 53,
+    psf_hlr: float = 0.7,
+):
+    gal = galsim.Gaussian(flux=f, half_light_radius=hlr)
+    gal = gal.shear(g1=e1, g2=e2)
+    gal = gal.shear(g1=g1, g2=g2)
+
+    psf = galsim.Gaussian(flux=1.0, half_light_radius=psf_hlr)
+    gal_conv = galsim.Convolve([gal, psf])
     image = gal_conv.drawImage(nx=slen, ny=slen, scale=pixel_scale, offset=(x, y))
     return image.array
