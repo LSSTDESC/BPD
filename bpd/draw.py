860adc88fee49bd9fed01a02977b3beb3dc8f457
ismael2395 <imendoza@umich.edu>
2024-10-22 17:54:06 -0700

    make fft a static arg and test we can still jit/vmap

--- bpd/draw.py
+++ bpd/draw.py
@@ -1,7 +1,6 @@
 import galsim
 import jax_galsim as xgalsim
-
-GSPARAMS = xgalsim.GSParams(minimum_fft_size=256, maximum_fft_size=256)
+from jax_galsim import GSParams
 
 
 def draw_gaussian(
@@ -16,14 +15,17 @@ def draw_gaussian(
     pixel_scale: float = 0.2,
     slen: int = 53,
     psf_hlr: float = 0.7,
+    fft_size: int = 256,
 ):
+    gsparams = GSParams(minimum_fft_size=fft_size, maximum_fft_size=fft_size)
+
     # x, y arguments in pixels
     gal = xgalsim.Gaussian(flux=f, half_light_radius=hlr)
     gal = gal.shear(g1=e1, g2=e2)
     gal = gal.shear(g1=g1, g2=g2)
 
     psf = xgalsim.Gaussian(flux=1, half_light_radius=psf_hlr)
-    gal_conv = xgalsim.Convolve([gal, psf]).withGSParams(GSPARAMS)
+    gal_conv = xgalsim.Convolve([gal, psf]).withGSParams(gsparams)
     image = gal_conv.drawImage(nx=slen, ny=slen, scale=pixel_scale, offset=(x, y))
     return image.array
 
