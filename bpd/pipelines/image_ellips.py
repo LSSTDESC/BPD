f81977376f2460129bc08fe9b9c9eb4c8031a2ab
ismael2395 <imendoza@umich.edu>
2024-10-23 10:43:17 -0700

    fixes to pipeline
    sigma_e_int added as argument that is taken by prior; also make function on a single galaxy, vmap for more

--- bpd/pipelines/image_ellips.py
+++ bpd/pipelines/image_ellips.py
@@ -5,7 +5,7 @@ import blackjax
 import jax.numpy as jnp
 from jax import Array
 from jax import jit as jjit
-from jax import random, vmap
+from jax import random
 from jax._src.prng import PRNGKeyArray
 from jax.scipy import stats
 
@@ -16,12 +16,57 @@ from bpd.pipelines.toy_ellips import do_inference
 from bpd.prior import ellip_mag_prior, sample_ellip_prior
 
 
+def get_target_galaxy_params_simple(
+    rng_key: PRNGKeyArray,
+    sigma_e: float = 1e-3,
+    lf: float = 3.0,
+    hlr: float = 1.0,
+    x: float = 0.0,  # pixels
+    y: float = 0.0,
+    g1: float = 0.02,
+    g2: float = 0.0,
+):
+    """Fix all parameters except ellipticity, which come from prior."""
+    e = sample_ellip_prior(rng_key, sigma=sigma_e, n=1)
+    return {
+        "lf": lf,
+        "hlr": hlr,
+        "e1": e[0, 0],
+        "e2": e[0, 1],
+        "x": x,
+        "y": y,
+        "g1": g1,
+        "g2": g2,
+    }
+
+
+def get_target_images_single(
+    rng_key: PRNGKeyArray,
+    n_samples: int,
+    single_galaxy_params: dict[str, float],
+    psf_hlr: float = 0.7,
+    background: float = 1.0,
+    slen: int = 53,
+    pixel_scale: float = 0.2,
+):
+    """In this case, we sample multiple noise realizations of the same galaxy."""
+    assert "f" in single_galaxy_params and "lf" not in single_galaxy_params
+
+    noiseless = draw_gaussian_galsim(
+        **single_galaxy_params,
+        pixel_scale=pixel_scale,
+        psf_hlr=psf_hlr,
+        slen=slen,
+    )
+    return add_noise(rng_key, noiseless, bg=background, n=n_samples), noiseless
+
+
 # interim prior
 def logprior(
     params: dict[str, Array],
     flux_bds: tuple = (-1.0, 9.0),
     hlr_bds: tuple = (0.01, 5.0),
-    sigma_e: float = 2e-3,
+    sigma_e: float = 1e-2,
     sigma_x: float = 1.0,  # pixels
 ) -> Array:
     prior = jnp.array(0.0)
@@ -73,7 +118,7 @@ def do_inference(
     is_mass_matrix_diagonal: bool = False,
     n_warmup_steps: int = 500,
     max_num_doublings: int = 5,
-    initial_step_size: float = 0.1,
+    initial_step_size: float = 1e-3,
     target_acceptance_rate: float = 0.80,
     n_samples: int = 100,
 ):
@@ -100,82 +145,37 @@ def do_inference(
     return states.position
 
 
-def get_target_galaxy_params_simple(
-    rng_key: PRNGKeyArray,
-    sigma_e: float = 1e-3,
-    lf: float = 3.0,
-    hlr: float = 1.0,
-    x: float = 0.0,  # pixels
-    y: float = 0.0,
-    g1: float = 0.02,
-    g2: float = 0.0,
-):
-    """Fix all parameters except ellipticity, which come from prior."""
-    e = sample_ellip_prior(rng_key, sigma=sigma_e, n=1)
-    return {
-        "lf": lf,
-        "hlr": hlr,
-        "e1": e[0, 0],
-        "e1": e[0, 1],
-        "x": x,
-        "y": y,
-        "g1": g1,
-        "g2": g2,
-    }
-
-
-def get_target_images_single(
-    rng_key: PRNGKeyArray,
-    n_samples: int,
-    single_galaxy_params: dict[str, float],
-    psf_hlr: float = 0.7,
-    background: float = 1.0,
-    slen: int = 53,
-    pixel_scale: float = 0.2,
-):
-    """In this case, we sample multiple noise realizations of the same galaxy."""
-    assert "f" in single_galaxy_params and "lf" not in single_galaxy_params
-
-    noiseless = draw_gaussian_galsim(
-        **single_galaxy_params,
-        pixel_scale=pixel_scale,
-        psf_hlr=psf_hlr,
-        slen=slen,
-    )
-    return add_noise(rng_key, noiseless, bg=background, n=n_samples)
-
-
 def pipeline_image_interim_samples(
-    key: PRNGKeyArray,
+    rng_key: PRNGKeyArray,
     true_params: dict[str, float],
-    images: Array,
+    target_image: Array,
     *,
     initialization_fnc: Callable,
+    sigma_e_int: float = 1e-2,
+    n_samples: int = 100,
     max_num_doublings: int = 5,
-    n_samples_per_galaxy: int = 100,
-    init_step_size: float = 0.1,
+    initial_step_size: float = 1e-3,
     target_acceptance_rate: float = 0.80,
     n_warmup_steps: int = 500,
     is_mass_matrix_diagonal: bool = False,
-    pixel_scale: float = 0.2,
     slen: int = 53,
+    pixel_scale: float = 0.2,
     psf_hlr: float = 0.7,
     background: float = 1.0,
 ):
 
-    n_galaxies = images.shape[0]
+    k1, k2 = random.split(rng_key)
 
-    k1, k2 = random.split(key)
-    keys2 = random.split(k2, n_galaxies)
-
-    init_positions = initialization_fnc(k1, true_params=true_params, data=images)
+    init_position = initialization_fnc(k1, true_params=true_params, data=target_image)
 
     _draw_fnc = partial(
         draw_gaussian, pixel_scale=pixel_scale, slen=slen, psf_hlr=psf_hlr
     )
     _loglikelihood = partial(loglikelihood, draw_fnc=_draw_fnc, background=background)
+    _logprior = partial(logprior, sigma_e=sigma_e_int)
+
     _logtarget = partial(
-        logtarget, logprior_fnc=logprior, loglikelihood_fnc=_loglikelihood
+        logtarget, logprior_fnc=_logprior, loglikelihood_fnc=_loglikelihood
     )
 
     _inference_fnc = partial(
@@ -184,13 +184,11 @@ def pipeline_image_interim_samples(
         is_mass_matrix_diagonal=is_mass_matrix_diagonal,
         n_warmup_steps=n_warmup_steps,
         max_num_doublings=max_num_doublings,
-        init_step_size=init_step_size,
+        initial_step_size=initial_step_size,
         target_acceptance_rate=target_acceptance_rate,
-        n_samples=n_samples_per_galaxy,
+        n_samples=n_samples,
     )
-    _do_inference_jitted = jjit(_inference_fnc)
-    _run_inference = vmap(_do_inference_jitted, (0, 0, 0))
+    _run_inference = jjit(_inference_fnc)
 
-    interim_samples = _run_inference(keys2, init_positions, images)
-
+    interim_samples = _run_inference(k2, init_position, target_image)
     return interim_samples
