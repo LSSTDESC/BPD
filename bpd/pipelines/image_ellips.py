915d2f164b876a88d9e5544d4f0827c85d458222
ismael2395 <imendoza@umich.edu>
2024-10-23 07:03:18 -0700

    first draft of pipeline to infer shear from images

---
+++ bpd/pipelines/image_ellips.py
@@ -0,0 +1,194 @@
+from functools import partial
+from typing import Callable
+
+import blackjax
+import jax.numpy as jnp
+from jax import Array
+from jax import jit as jjit
+from jax import random, vmap
+from jax._src.prng import PRNGKeyArray
+from jax.scipy import stats
+
+from bpd.chains import inference_loop
+from bpd.draw import draw_gaussian, draw_gaussian_galsim
+from bpd.noise import add_noise
+from bpd.pipelines.toy_ellips import do_inference
+from bpd.prior import ellip_mag_prior, sample_ellip_prior
+
+
+# interim prior
+def logprior(
+    params: dict[str, Array],
+    flux_bds: tuple = (-1.0, 9.0),
+    hlr_bds: tuple = (0.01, 5.0),
+    sigma_e: float = 2e-3,
+    sigma_x: float = 1.0,  # pixels
+) -> Array:
+    prior = jnp.array(0.0)
+
+    f1, f2 = flux_bds
+    prior += stats.uniform.logpdf(params["f"], f1, f2 - f1)
+
+    h1, h2 = hlr_bds
+    prior += stats.uniform.logpdf(params["hlr"], h1, h2 - h1)
+
+    e_mag = jnp.sqrt(params["e1"] ** 2 + params["e2"] ** 2)
+    prior += jnp.log(ellip_mag_prior(e_mag, sigma=sigma_e))
+
+    # NOTE: hard-coded assumption that galaxy is always centered in odd-size image.
+    prior += stats.norm.logpdf(params["x"], loc=0.0, scale=sigma_x)
+    prior += stats.norm.logpdf(params["y"], loc=0.0, scale=sigma_x)
+
+    return prior
+
+
+def loglikelihood(
+    params: dict[str, Array], data: Array, *, draw_fnc: Callable, background: float
+):
+    _full_params = {**{"g1": 0.0, "g2": 0.0}, **params}  # function is more general
+    _full_params["f"] = 10 ** _full_params["f"]  # log-flux
+
+    model = draw_fnc(**_full_params)
+    likelihood_pp = stats.norm.logpdf(data, loc=model, scale=jnp.sqrt(background))
+    likelihood = jnp.sum(likelihood_pp)
+    return likelihood
+
+
+def logtarget(
+    params: dict[str, Array],
+    data: Array,
+    logprior_fnc: Callable,
+    loglikelihood_fnc: Callable,
+):
+    return logprior_fnc(params) + loglikelihood_fnc(params, data)
+
+
+def do_inference(
+    rng_key: PRNGKeyArray,
+    init_positions: dict[str, Array],
+    data: Array,
+    *,
+    logtarget_fnc: Callable,
+    is_mass_matrix_diagonal: bool = False,
+    n_warmup_steps: int = 500,
+    max_num_doublings: int = 5,
+    initial_step_size: float = 0.1,
+    target_acceptance_rate: float = 0.80,
+    n_samples: int = 100,
+):
+
+    key1, key2 = random.split(rng_key)
+
+    _logdensity = partial(logtarget_fnc, data=data)
+
+    warmup = blackjax.window_adaptation(
+        blackjax.nuts,
+        _logdensity,
+        progress_bar=False,
+        is_mass_matrix_diagonal=is_mass_matrix_diagonal,
+        max_num_doublings=max_num_doublings,
+        initial_step_size=initial_step_size,
+        target_acceptance_rate=target_acceptance_rate,
+    )
+
+    (init_states, tuned_params), _ = warmup.run(key1, init_positions, n_warmup_steps)
+
+    kernel = blackjax.nuts(_logdensity, **tuned_params).step
+    states, _ = inference_loop(key2, init_states, kernel=kernel, n_samples=n_samples)
+
+    return states.position
+
+
+def get_target_galaxy_params_simple(
+    rng_key: PRNGKeyArray,
+    sigma_e=0.3,
+    f: float = 1000.0,
+    hlr: float = 1.0,
+    x: float = 0.0,  # pixels
+    y: float = 0.0,
+    g1: float = 0.02,
+    g2: float = 0.0,
+):
+    """Fix all parameters except ellipticity, which come from prior."""
+    e = sample_ellip_prior(rng_key, sigma=sigma_e, n=1)
+    return {
+        "f": f,
+        "hlr": hlr,
+        "e1": e[0, 0],
+        "e1": e[0, 1],
+        "x": x,
+        "y": y,
+        "g1": g1,
+        "g2": g2,
+    }
+
+
+def get_target_images_single(
+    rng_key: PRNGKeyArray,
+    n_samples: int,
+    single_galaxy_params: dict[str, float],
+    psf_hlr: float = 0.7,
+    background: float = 1.0,
+    slen: int = 53,
+    pixel_scale: float = 0.2,
+):
+    """In this case, we sample multiple noise realizations of the same galaxy."""
+
+    noiseless = draw_gaussian_galsim(
+        **single_galaxy_params,
+        pixel_scale=pixel_scale,
+        psf_hlr=psf_hlr,
+        slen=slen,
+    )
+    return add_noise(rng_key, noiseless, bg=background, n=n_samples)
+
+
+# TODO: avoid vmapping over assumed shear?
+def pipeline_interim_samples(
+    key: PRNGKeyArray,
+    images: Array,
+    *,
+    initialization_fnc: Callable,
+    max_num_doublings: int = 5,
+    n_samples_per_galaxy: int = 100,
+    init_step_size: float = 0.1,
+    target_acceptance_rate: float = 0.80,
+    n_warmup_steps: int = 500,
+    is_mass_matrix_diagonal: bool = False,
+    pixel_scale: float = 0.2,
+    slen: int = 53,
+    psf_hlr: float = 0.7,
+    background: float = 1.0,
+):
+
+    n_galaxies = images.shape[0]
+
+    k1, k2 = random.split(key)
+    keys2 = random.split(k2, n_galaxies)
+
+    init_positions = initialization_fnc(k1, images)
+
+    _draw_fnc = partial(
+        draw_gaussian, pixel_scale=pixel_scale, slen=slen, psf_hlr=psf_hlr
+    )
+    _loglikelihood = partial(loglikelihood, draw_fnc=_draw_fnc, background=background)
+    _logtarget = partial(
+        logtarget, logprior_fnc=logprior, loglikelihood_fnc=_loglikelihood
+    )
+
+    _inference_fnc = partial(
+        do_inference,
+        logtarget_fnc=_logtarget,
+        is_mass_matrix_diagonal=is_mass_matrix_diagonal,
+        n_warmup_steps=n_warmup_steps,
+        max_num_doublings=max_num_doublings,
+        init_step_size=init_step_size,
+        target_acceptance_rate=target_acceptance_rate,
+        n_samples=n_samples_per_galaxy,
+    )
+    _do_inference_jitted = jjit(_inference_fnc)
+    _run_inference = vmap(_do_inference_jitted, (0, 0, 0))
+
+    interim_samples = _run_inference(keys2, init_positions, images)
+
+    return interim_samples
