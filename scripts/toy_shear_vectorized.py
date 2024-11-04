12eb2157d238fb97ba9955973dedac41e5bfd315
ismael2395 <imendoza@umich.edu>
2024-11-04 12:09:04 -0800

    rename and use typer

--- scripts/toy_shear_vectorized.py
+++ scripts/toy_shear_vectorized.py
@@ -2,8 +2,8 @@
 from functools import partial
 from math import ceil
 
-import click
 import jax.numpy as jnp
+import typer
 from jax import random, vmap
 
 from bpd import DATA_DIR
@@ -11,38 +11,24 @@ from bpd.pipelines.shear_inference import pipeline_shear_inference
 from bpd.pipelines.toy_ellips import pipeline_toy_ellips_samples
 
 
-@click.command()
-@click.option("--n-vec", type=int, default=50, help="# shear chains in parallel")
-@click.option("--tag", type=str, required=True)
-@click.option("--seed", type=int, required=True)
-@click.option("--n-seeds", type=int, required=True)
-@click.option("--g1", type=float, default=0.02)
-@click.option("--g2", type=float, default=0.0)
-@click.option("--n-samples-gals", type=int, default=1000, help="# of gals")
-@click.option("--n-samples-shear", type=int, default=3000, help="shear samples")
-@click.option("--k", type=int, default=1000, help="# int. post. samples galaxy.")
-@click.option("--shape-noise", type=float, default=1e-3)
-@click.option("--sigma-e-int", type=float, default=3e-2)
-@click.option("--obs-noise", type=float, default=1e-4)
-@click.option("--trim", type=int, default=10)
 def main(
-    n_vec: int,
     tag: str,
-    seed: int,
-    n_seeds: int,
-    g1: float,
-    g2: float,
-    n_samples_gals: int,
-    n_samples_shear: int,
-    k: int,
-    shape_noise: float,
-    sigma_e_int: float,
-    obs_noise: float,
-    trim: int,
+    base_seed: int,
+    n_exp: int,
+    shape_noise: float = 1e-3,
+    obs_noise: float = 1e-4,
+    sigma_e_int: float = 3e-2,
+    n_vec: int = 50,
+    g1: float = 0.02,
+    g2: float = 0.0,
+    n_samples_gals: int = 1000,
+    n_samples_shear: int = 3000,
+    k: int = 1000,
+    trim: int = 10,
 ):
-    key0 = random.key(seed)
-    _keys = random.split(key0, n_seeds * 2)  # one for toy ellipticities, one for shear
-    keys = _keys.reshape(n_seeds, 2)
+    key0 = random.key(base_seed)
+    _keys = random.split(key0, n_exp * 2)  # one for toy ellipticities, one for shear
+    keys = _keys.reshape(n_exp, 2)
 
     # directory structure
     dirpath = DATA_DIR / "cache_chains" / tag
@@ -84,8 +70,8 @@ def main(
 
         g_samples = vpipe2(skeys, e_post_trimmed)
 
-        fpath_ellip = dirpath / f"e_post_{seed}_{ii}.npy"
-        fpath_shear = dirpath / f"g_samples_{seed}_{ii}.npy"
+        fpath_ellip = dirpath / f"e_post_{base_seed}_{ii}.npy"
+        fpath_shear = dirpath / f"g_samples_{base_seed}_{ii}.npy"
 
         assert not fpath_shear.exists()
         jnp.save(fpath_ellip, e_post)
@@ -93,4 +79,4 @@ def main(
 
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
