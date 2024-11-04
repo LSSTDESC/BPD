29abcda4cf1caae2d982b73096a71025ee302f25
ismael2395 <imendoza@umich.edu>
2024-11-04 10:06:46 -0800

    being careful with defaults and renaming

--- bpd/pipelines/toy_ellips.py
+++ bpd/pipelines/toy_ellips.py
@@ -7,7 +7,7 @@ from jax import Array, random, vmap
 from jax import jit as jjit
 from jax._src.prng import PRNGKeyArray
 
-from bpd.chains import do_inference_nuts
+from bpd.chains import run_inference_nuts
 from bpd.prior import ellip_mag_prior, sample_synthetic_sheared_ellips_unclipped
 
 
@@ -32,38 +32,39 @@ def logtarget(
 
 def pipeline_toy_ellips_samples(
     key: PRNGKeyArray,
+    *,
     g1: float,
     g2: float,
     sigma_e: float,
     sigma_e_int: float,
     sigma_m: float,
-    n_samples: int,
-    k: int,
+    n_gals: int,
+    n_samples_per_gal: int,
     n_warmup_steps: int = 500,
+    max_num_doublings: int = 2,
 ):
     k1, k2 = random.split(key)
 
     true_g = jnp.array([g1, g2])
 
     e_obs, e_sheared, _ = sample_synthetic_sheared_ellips_unclipped(
-        k1, true_g, n=n_samples, sigma_m=sigma_m, sigma_e=sigma_e
+        k1, true_g, n=n_gals, sigma_m=sigma_m, sigma_e=sigma_e
     )
 
     interim_prior = partial(ellip_mag_prior, sigma=sigma_e_int)
 
     _logtarget = partial(logtarget, sigma_m=sigma_m, interim_prior=interim_prior)
 
-    keys2 = random.split(k2, n_samples)
+    keys2 = random.split(k2, n_gals)
     _do_inference_jitted = jjit(
         partial(
-            do_inference_nuts,
+            run_inference_nuts,
             logtarget=_logtarget,
-            n_samples=k,
-            sigma_m=sigma_m,
+            n_samples=n_samples_per_gal,
             initial_step_size=sigma_e,
             interim_prior=interim_prior,
             n_warmup_steps=n_warmup_steps,
-            max_num_doublins=2,
+            max_num_doublins=max_num_doublings,
         )
     )
     _do_inference = vmap(_do_inference_jitted, in_axes=(0, 0, 0))
