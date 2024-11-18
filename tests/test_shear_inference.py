767d91903b5fcaf209c53f34d3ae61044b9a2761
ismael2395 <imendoza@umich.edu>
2024-11-18 09:18:39 -0800

    added new test on scaling of scatter

--- tests/test_shear_inference.py
+++ tests/test_shear_inference.py
@@ -6,6 +6,7 @@ from jax import random
 
 from bpd.pipelines.shear_inference import pipeline_shear_inference
 from bpd.pipelines.toy_ellips import pipeline_toy_ellips_samples
+from bpd.prior import sample_ellip_prior
 
 
 @pytest.mark.parametrize("seed", [1234, 4567])
@@ -44,3 +45,43 @@ def test_shear_inference_toy_ellipticities(seed):
     assert shear_samples.shape == (1000, 2)
     assert jnp.abs((jnp.mean(shear_samples[:, 0]) - g1) / g1) <= 3e-3
     assert jnp.abs(jnp.mean(shear_samples[:, 1])) <= 3e-3
+
+
+@pytest.mark.parametrize("seed", [1234, 4567])
+def check_shape_noise_scaling(seed):
+    """No measure noise is added so scaling of shear scatter should be ~`shape_noise / sqrt(N)`."""
+    key = random.key(seed)
+    k1, k2 = random.split(key)
+
+    g1 = 0.02
+    g2 = 0.0
+    sigma_e = 1e-3
+    sigma_e_int = 3e-2
+    n_gals = 1000
+
+    # no observation noise, 1 sample.
+    e_post = sample_ellip_prior(k1, sigma=sigma_e, n=n_gals).reshape(n_gals, 1, 2)
+
+    shear_samples = pipeline_shear_inference(
+        k2,
+        e_post,
+        true_g=jnp.array([g1, g2]),
+        sigma_e=sigma_e,
+        sigma_e_int=sigma_e_int,
+        n_samples=2000,
+        initial_step_size=1e-3,
+    )
+    assert shear_samples.shape == (2000, 2)
+    g1_mean = jnp.mean(shear_samples[:, 0])
+    g2_mean = jnp.mean(shear_samples[:, 1])
+    g1_std = jnp.std(shear_samples[:, 0])
+    g2_std = jnp.std(shear_samples[:, 1])
+
+    # mean check
+    assert jnp.abs((g1_mean - g1) / g1) <= 3e-3
+    assert jnp.abs(g2_mean) <= 3e-3
+
+    # scatter
+    # NOTE: sqrt(2) factor or no??
+    assert jnp.allclose(g1_std, sigma_e / jnp.sqrt(n_gals), atol=0, rtol=2e-2)
+    assert jnp.allclose(g2_std, sigma_e / jnp.sqrt(n_gals), atol=0, rtol=2e-2)
