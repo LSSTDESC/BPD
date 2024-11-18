03b7ca48bc0e9dcc30e7f1d235ccd2fff45505ac
ismael2395 <imendoza@umich.edu>
2024-11-18 13:46:33 -0800

    sometimes it is useful to split them up

--- bpd/chains.py
+++ bpd/chains.py
@@ -21,6 +21,51 @@ def inference_loop(rng_key, initial_state, kernel, n_samples: int):
     return (states, infos)
 
 
+def run_warmup_nuts(
+    rng_key: PRNGKeyArray,
+    init_positions: ArrayLike,
+    data: ArrayLike,
+    *,
+    logtarget: Callable,
+    initial_step_size: float,
+    max_num_doublings: int,
+    n_warmup_steps: int = 500,
+    is_mass_matrix_diagonal: bool = True,
+    target_acceptance_rate: float = 0.8,
+):
+    _logtarget = partial(logtarget, data=data)
+    warmup = blackjax.window_adaptation(
+        blackjax.nuts,
+        _logtarget,
+        progress_bar=False,
+        is_mass_matrix_diagonal=is_mass_matrix_diagonal,
+        max_num_doublings=max_num_doublings,
+        initial_step_size=initial_step_size,
+        target_acceptance_rate=target_acceptance_rate,
+    )
+
+    (init_states, tuned_params), _ = warmup.run(rng_key, init_positions, n_warmup_steps)
+    return init_states, tuned_params
+
+
+def run_sampling_nuts(
+    rng_key: PRNGKeyArray,
+    init_states: ArrayLike,
+    tuned_params: dict,
+    data: ArrayLike,
+    *,
+    logtarget: Callable,
+    n_samples: int,
+    max_num_doublings=5,
+):
+    _logtarget = partial(logtarget, data=data)
+    kernel = blackjax.nuts(
+        _logtarget, **tuned_params, max_num_doublings=max_num_doublings
+    ).step
+    states, _ = inference_loop(rng_key, init_states, kernel=kernel, n_samples=n_samples)
+    return states.position
+
+
 def run_inference_nuts(
     rng_key: PRNGKeyArray,
     init_positions: ArrayLike,
