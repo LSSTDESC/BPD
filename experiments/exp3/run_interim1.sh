7fd37cdc17f5874bc9fa784b6808ceed90acb731
ismael2395 <imendoza@umich.edu>
2024-11-20 10:13:38 -0800

    rename

--- experiments/exp3/run_interim1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export CUDA_VISIBLE_DEVICES="0"
-
-
-./get_image_interim_samples_fixed1.py 43 interim_image_fix3  --n-gals 500 --n-vec 100
-./get_shear_from_interim_samples.py 43 interim_image_fix3 "e_post_43.npz"
