dee9126775ba29982c4f22a64bf7a3106733fb4c
Ismael Mendoza <imendoza@umich.edu>
2024-11-30 11:49:40 -0500

    refactor to use arrays and vmap

--- bpd/prior.py
+++ bpd/prior.py
@@ -1,5 +1,5 @@
 import jax.numpy as jnp
-from jax import Array, random
+from jax import Array, random, vmap
 from jax._src.prng import PRNGKeyArray
 from jax.numpy.linalg import norm
 from jaxtyping import ArrayLike
@@ -46,6 +46,8 @@ def scalar_shear_transformation(e: Array, g: Array):
 
     NOTE: This function is meant to be vmapped later.
     """
+    assert e.shape == (2,) and g.shape == (2,)
+
     e1, e2 = e
     g1, g2 = g
 
@@ -53,11 +55,12 @@ def scalar_shear_transformation(e: Array, g: Array):
     g_comp = g1 + g2 * 1j
 
     e_prime = (e_comp + g_comp) / (1 + g_comp.conjugate() * e_comp)
-    return e_prime.real, e_prime.imag
+    return jnp.array([e_prime.real, e_prime.imag])
 
 
 def scalar_inv_shear_transformation(e: Array, g: Array):
     """Same as above but the inverse."""
+    assert e.shape == (2,) and g.shape == (2,)
     e1, e2 = e
     g1, g2 = g
 
@@ -65,39 +68,16 @@ def scalar_inv_shear_transformation(e: Array, g: Array):
     g_comp = g1 + g2 * 1j
 
     e_prime = (e_comp - g_comp) / (1 - g_comp.conjugate() * e_comp)
-    return e_prime.real, e_prime.imag
+    return jnp.array([e_prime.real, e_prime.imag])
 
+
+# batched
+shear_transformation = vmap(scalar_shear_transformation, in_axes=(0, None))
+inv_shear_transformation = vmap(scalar_inv_shear_transformation, in_axes=(0, None))
 
 # useful for jacobian later
 inv_shear_func1 = lambda e, g: scalar_inv_shear_transformation(e, g)[0]
 inv_shear_func2 = lambda e, g: scalar_inv_shear_transformation(e, g)[1]
-
-
-def shear_transformation(e: Array, g: Array):
-    """Transform elliptiticies by a fixed shear.
-
-    The transformation we used is equation 3.4b in Seitz & Schneider (1997).
-    """
-    e1, e2 = e[..., 0], e[..., 1]
-    g1, g2 = g
-
-    e_comp = e1 + e2 * 1j
-    g_comp = g1 + g2 * 1j
-
-    e_prime = (e_comp + g_comp) / (1 + g_comp.conjugate() * e_comp)
-    return jnp.stack([e_prime.real, e_prime.imag], axis=-1)
-
-
-def inv_shear_transformation(e: Array, g: Array):
-    """Same as above but the inverse."""
-    e1, e2 = e[..., 0], e[..., 1]
-    g1, g2 = g
-
-    e_comp = e1 + e2 * 1j
-    g_comp = g1 + g2 * 1j
-
-    e_prime = (e_comp - g_comp) / (1 - g_comp.conjugate() * e_comp)
-    return jnp.stack([e_prime.real, e_prime.imag], axis=-1)
 
 
 # get synthetic measured sheared ellipticities
