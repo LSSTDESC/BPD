0558d995aa47f9990d5ea042c539fec44c27d280
ismael2395 <imendoza@umich.edu>
2024-12-11 19:54:40 -0800

    refactor to allow more parameters in prior

--- bpd/likelihood.py
+++ bpd/likelihood.py
@@ -1,4 +1,3 @@
-from functools import partial
 from typing import Callable
 
 import jax.numpy as jnp
@@ -7,43 +6,46 @@ from jax import Array, grad, vmap
 from jax.numpy.linalg import norm
 from jax.typing import ArrayLike
 
-from bpd.prior import inv_shear_func1, inv_shear_func2, inv_shear_transformation
+from bpd.prior import (
+    ellip_prior_e1e2,
+    inv_shear_func1,
+    inv_shear_func2,
+    inv_shear_transformation,
+)
 
 _grad_fnc1 = vmap(vmap(grad(inv_shear_func1), in_axes=(0, None)), in_axes=(0, None))
 _grad_fnc2 = vmap(vmap(grad(inv_shear_func2), in_axes=(0, None)), in_axes=(0, None))
 _inv_shear_trans = vmap(inv_shear_transformation, in_axes=(0, None))
 
 
-def shear_loglikelihood(
-    g: Array,
-    sigma_e: float,
-    e_post: Array,
-    *,
-    prior: Callable,
-    interim_prior: Callable,  # fixed
-) -> ArrayLike:
-    # Given by the inference procedure in Schneider et al. 2014
-    # assume single shear g
-    # assume e_obs.shape == (N, K, 2) where N is number of galaxies, K is samples per galaxy
-    # the priors are callables for now on only ellipticities
-    # the interim_prior should have been used when obtaining e_obs from the chain (i.e. for now same sigma)
-    _, _, _ = e_post.shape  # (N, K, 2)
-    _prior = partial(prior, sigma=sigma_e)
+def true_ellip_logprior(e_post: Array, g: Array, *, sigma_e: float):
+    """Implementation of GB's true prior on interim posterior samples of ellipticities."""
 
-    e_post_mag = norm(e_post, axis=-1)
-    denom = interim_prior(e_post_mag)  # (N, K), can ignore angle in prior as uniform
-
-    # for num, use trick
-    # p(w_n' | g, alpha )  = p(w_n' \cross^{-1} g | alpha ) = p(w_n | alpha) * |jac(w_n / w_n')|
-
-    # shape = (N, K, 2)
+    # jacobian of inverse shear transformation
     grad1 = _grad_fnc1(e_post, g)
     grad2 = _grad_fnc2(e_post, g)
     absjacdet = jnp.abs(grad1[..., 0] * grad2[..., 1] - grad1[..., 1] * grad2[..., 0])
 
+    # true prior on unsheared ellipticity
     e_post_unsheared = _inv_shear_trans(e_post, g)
-    e_post_unsheared_mag = norm(e_post_unsheared, axis=-1)
-    num = _prior(e_post_unsheared_mag) * absjacdet  # (N, K)
+    prior_val = ellip_prior_e1e2(e_post_unsheared, sigma=sigma_e)
 
-    ratio = jsp.special.logsumexp(jnp.log(num) - jnp.log(denom), axis=-1)
+    return jnp.log(prior_val) + jnp.log(absjacdet)
+
+
+def shear_loglikelihood(
+    g: Array,
+    post_params: dict[str, Array],
+    *,
+    logprior: Callable,
+    interim_logprior: Callable,  # fixed
+) -> ArrayLike:
+    """Shear Likelihood implementation of Schneider et al. 2014."""
+    e_post = post_params["e1e2"]
+    _, _, _ = e_post.shape  # (N, K, 2)
+
+    denom = interim_logprior(post_params)
+    num = logprior(post_params, g)
+
+    ratio = jsp.special.logsumexp(num - denom, axis=-1)
     return ratio.sum()
