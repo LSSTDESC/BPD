58f91f736856a004b1e863e42e182a74fbe1d55b
ismael2395 <imendoza@umich.edu>
2025-01-20 14:10:14 -0800

    no jack

--- experiments/exp40/get_shear_posteriors.sh
+++ experiments/exp40/get_shear_posteriors.sh
@@ -4,4 +4,4 @@ SEED="42"
  
 ../../scripts/get_shear_from_shapes.py $SEED --old-seed $SEED --interim-samples-fname "interim_samples_${SEED}_plus.npz" --tag exp40_$SEED --overwrite --extra-tag "plus"
 ../../scripts/get_shear_from_shapes.py $SEED --old-seed $SEED --interim-samples-fname "interim_samples_${SEED}_minus.npz" --tag exp40_$SEED --overwrite --extra-tag "minus"
-./get_shear_jackknife.py $SEED --old-seed $SEED --samples-plus-fname "interim_samples_${SEED}_plus.npz" --samples-minus-fname "interim_samples_${SEED}_minus.npz" --tag exp40_$SEED --overwrite --n-jacks 50
+# ./get_shear_jackknife.py $SEED --old-seed $SEED --samples-plus-fname "interim_samples_${SEED}_plus.npz" --samples-minus-fname "interim_samples_${SEED}_minus.npz" --tag exp40_$SEED --overwrite --n-jacks 50
