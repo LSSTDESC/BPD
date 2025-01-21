6633ec06b9c130e83f17cd4981e2d3fcf532ad7f
ismael2395 <imendoza@umich.edu>
2025-01-21 07:57:07 -0800

    make file absorbts this

--- experiments/exp40/get_samples.sh
+++
@@ -1,2 +0,0 @@
-./slurm_get_interim_samples.py 42 --g1 0.02 --g2 0.0 --mode "plus"
-./slurm_get_interim_samples.py 42 --g1 -0.02 --g2 0.0 --mode "minus"
--- experiments/exp40/get_shear_posteriors.sh
+++
@@ -1,7 +0,0 @@
-export CUDA_VISIBLE_DEVICES="0"
-export JAX_ENABLE_X64="True"
-SEED="42"
- 
-../../scripts/get_shear_from_shapes.py $SEED --old-seed $SEED --interim-samples-fname "interim_samples_${SEED}_plus.npz" --tag exp40_$SEED --overwrite --extra-tag "plus"
-../../scripts/get_shear_from_shapes.py $SEED --old-seed $SEED --interim-samples-fname "interim_samples_${SEED}_minus.npz" --tag exp40_$SEED --overwrite --extra-tag "minus"
-# ./get_shear_jackknife.py $SEED --old-seed $SEED --samples-plus-fname "interim_samples_${SEED}_plus.npz" --samples-minus-fname "interim_samples_${SEED}_minus.npz" --tag exp40_$SEED --overwrite --n-jacks 50
