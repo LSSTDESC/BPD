0edfb898c19f0874c3033d77f199eb6ced514802
ismael2395 <imendoza@umich.edu>
2025-02-05 11:28:33 -0800

    fix ordering

--- experiments/exp40/collate_samples.py
+++ experiments/exp40/collate_samples.py
@@ -19,21 +19,27 @@ def main(seed: int, tag: str = typer.Option(), mode: str = typer.Option()):
 
     full_ds = {}
 
+    # first collect all relevant files
+    fps = []
     for fp in dirpath.iterdir():
         cond1 = fp.name.startswith(f"interim_samples_{seed}")
         cond2 = fp.name != newpath.name
         cond3 = mode in fp.name
         if cond1 and cond2 and cond3:
-            ds = load_dataset(fp)
+            fps.append(fp)
 
-            for k, v in ds.items():
-                if k in ("e_post", "e1_true", "e2_true", "f"):
-                    if k in full_ds:
-                        full_ds[k] = jnp.concatenate([full_ds[k], v])
-                    else:
-                        full_ds[k] = ds[k]
+    fps = sorted(fps)  # paths have intrinsic ordering just like strings
+    for fp in fps:
+        ds = load_dataset(fp)
+
+        for k, v in ds.items():
+            if k in ("e_post", "e1_true", "e2_true", "f"):
+                if k in full_ds:
+                    full_ds[k] = jnp.concatenate([full_ds[k], v])
                 else:
                     full_ds[k] = ds[k]
+            else:
+                full_ds[k] = ds[k]
 
         save_dataset(full_ds, newpath, overwrite=True)
 
