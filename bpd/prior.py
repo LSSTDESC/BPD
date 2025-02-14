34ed3bb0a3ef5af1d37f49dec47838db0ec6375c
ismael2395 <imendoza@umich.edu>
2025-02-14 15:28:58 -0800

    reusable very probably

--- bpd/prior.py
+++ bpd/prior.py
@@ -48,21 +48,6 @@ _grad_fnc2 = vmap(vmap(grad(inv_shear_func2), in_axes=(0, None)), in_axes=(0, No
 _inv_shear_trans = vmap(inv_shear_transformation, in_axes=(0, None))
 
 
-def true_ellip_logprior(e_post: Array, g: Array, *, sigma_e: float):
-    """Implementation of GB's true prior on interim posterior samples of ellipticities."""
-
-    # jacobian of inverse shear transformation
-    grad1 = _grad_fnc1(e_post, g)
-    grad2 = _grad_fnc2(e_post, g)
-    absjacdet = jnp.abs(grad1[..., 0] * grad2[..., 1] - grad1[..., 1] * grad2[..., 0])
-
-    # true prior on unsheared ellipticity
-    e_post_unsheared = _inv_shear_trans(e_post, g)
-    prior_val = ellip_prior_e1e2(e_post_unsheared, sigma=sigma_e)
-
-    return jnp.log(prior_val) + jnp.log(absjacdet)
-
-
 def interim_gprops_logprior(
     params: dict[str, Array],
     *,
@@ -88,5 +73,46 @@ def interim_gprops_logprior(
 
     e1e2 = jnp.stack((params["e1"], params["e2"]), axis=-1)
     prior += jnp.log(ellip_prior_e1e2(e1e2, sigma=sigma_e))
+
+    return prior
+
+
+def true_ellip_logprior(e_post: Array, g: Array, *, sigma_e: float):
+    """Implementation of GB's true prior on interim posterior samples of ellipticities."""
+
+    # jacobian of inverse shear transformation
+    grad1 = _grad_fnc1(e_post, g)
+    grad2 = _grad_fnc2(e_post, g)
+    absjacdet = jnp.abs(grad1[..., 0] * grad2[..., 1] - grad1[..., 1] * grad2[..., 0])
+
+    # true prior on unsheared ellipticity
+    e_post_unsheared = _inv_shear_trans(e_post, g)
+    prior_val = ellip_prior_e1e2(e_post_unsheared, sigma=sigma_e)
+
+    return jnp.log(prior_val) + jnp.log(absjacdet)
+
+
+def true_all_params_logprior(
+    post_params: dict[str, Array],
+    g: Array,
+    *,
+    sigma_e: float,
+    mean_logflux: float,
+    sigma_logflux: float,
+    mean_loghlr: float,
+    sigma_loghlr: float,
+):
+    lf = post_params["lf"]
+    lhlr = post_params["lhlr"]
+    e1e2 = jnp.stack((post_params["e1"], post_params["e2"]), axis=-1)
+
+    prior = jnp.array(0.0)
+
+    # we also pickup a jacobian in the corresponding probability densities
+    prior += stats.norm.logpdf(lf, loc=mean_logflux, scale=sigma_logflux)
+    prior += stats.norm.logpdf(lhlr, loc=mean_loghlr, scale=sigma_loghlr)
+
+    # elliptcity
+    prior += true_ellip_logprior(e1e2, g, sigma_e=sigma_e)
 
     return prior
