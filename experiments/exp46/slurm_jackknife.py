87033203ec4c98b232ae1df35443ebac68168db8
ismael2395 <imendoza@umich.edu>
2025-02-17 13:43:17 -0800

    ready to run

--- experiments/exp46/slurm_jackknife.py
+++ experiments/exp46/slurm_jackknife.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
 
+import typer
+
 from bpd.slurm import run_single_gpu_job
 
 
 def main(
     seed: int,
-    tag: str,
-    time: str = "02:00",  # HH:MM
+    tag: str = typer.Option(),
+    time: str = "04:00",  # HH:MM
     mem_per_gpu: str = "10G",
     qos: str = "regular",
 ):
@@ -25,10 +27,11 @@ def main(
     run_single_gpu_job(
         cmd,
         jobname=tag,
-        base_seed=seed,
         time=time,
         mem_per_gpu=mem_per_gpu,
         qos=qos,
-        nodes=1,
-        n_tasks_per_node=1,
     )
+
+
+if __name__ == "__main__":
+    typer.run(main)
