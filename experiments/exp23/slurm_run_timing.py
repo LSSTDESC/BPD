48e3a4001eac2b272d925aedc0a302cdf2d0255e
ismael2395 <imendoza@umich.edu>
2025-03-17 13:32:57 -0700

    slurm

---
+++ experiments/exp23/slurm_run_timing.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+import typer
+
+from bpd.slurm import run_single_gpu_job
+
+
+def main(
+    seed: int,
+    tag: str,
+    time: str = "02:00",  # HH:MM
+    mem_per_gpu: str = "20G",
+    qos: str = "regular",
+):
+    cmd = f"python /global/u2/i/imendoza/BPD/experiments/exp23/get_samples_timing.py {seed} {tag}"
+    run_single_gpu_job(
+        cmd,
+        jobname=tag,
+        time=time,
+        mem_per_gpu=mem_per_gpu,
+        qos=qos,
+    )
+
+
+if __name__ == "__main__":
+    typer.run(main)
