49f5787767160557946fdbdf1d1c1bbe520446b1
ismael2395 <imendoza@umich.edu>
2025-03-19 13:21:37 -0700

    slurm version

---
+++ experiments/exp70/slurm_bootstrap.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+
+import math
+
+import typer
+
+from bpd.slurm import run_multi_gpu_job
+
+
+def main(
+    seed: int,
+    tag: str = typer.Option(),
+    n_boots: int = 1000,
+    n_nodes: int = 5,
+    time: str = "00:30",  # HH:MM
+    mem_per_gpu: str = "10G",
+    qos: str = "debug",
+):
+    n_splits = n_nodes * 4
+    split_size = math.ceil(n_boots / n_splits)
+
+    cmds = []
+    for ii in range(n_splits):
+        base_cmd = """../exp70/simple_bootstrap.py {new_seed}
+        --samples-plus-fname interim_samples_{seed}_plus.npz
+        --samples-minus-fname interim_samples_{seed}_minus.npz
+        --tag {tag} --n-boots {split_size} --overwrite
+        """
+        base_cmd = " ".join(base_cmd.split())
+        new_seed = f"{seed}{ii}"
+        cmd = base_cmd.format(
+            new_seed=new_seed, seed=seed, tag=tag, split_size=split_size
+        )
+        cmds.append(cmd)
+
+    run_multi_gpu_job(
+        cmds,
+        jobname=f"{tag}_boot",
+        time=time,
+        mem_per_gpu=mem_per_gpu,
+        qos=qos,
+        nodes=n_nodes,
+    )
+
+
+if __name__ == "__main__":
+    typer.run(main)
