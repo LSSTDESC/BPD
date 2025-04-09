eb9bc83f6d7a1581118f6f22714cbed62e9a70c6
ismael2395 <imendoza@umich.edu>
2025-04-09 10:27:26 -0700

    initialize true prior parameters not at truth

--- scripts/get_shear_and_sn_from_shapes.py
+++ scripts/get_shear_and_sn_from_shapes.py
@@ -2,11 +2,12 @@
 """This file creates toy samples of ellipticities and saves them to .hdf5 file."""
 
 from functools import partial
+from pathlib import Path
 
 import jax
 import jax.numpy as jnp
 import typer
-from jax import jit
+from jax import jit, random
 
 from bpd import DATA_DIR
 from bpd.chains import run_inference_nuts
@@ -16,7 +17,7 @@ from bpd.pipelines import logtarget_shear_and_sn
 
 def main(
     seed: int,
-    samples_fname: str = typer.Option(),
+    samples_fpath: str = typer.Option(),
     tag: str = typer.Option(),
     initial_step_size: float = 0.01,
     n_samples: int = 3000,
@@ -28,21 +29,30 @@ def main(
     dirpath = DATA_DIR / "cache_chains" / tag
     if not dirpath.exists():
         dirpath.mkdir(exist_ok=True)
-    interim_samples_fpath = DATA_DIR / "cache_chains" / tag / samples_fname
-    assert interim_samples_fpath.exists(), "ellipticity samples file does not exist"
+
+    assert Path(samples_fpath).exists(), "ellipticity samples file does not exist"
     fpath = DATA_DIR / "cache_chains" / tag / f"shear_samples_{seed}{extra_txt}.npz"
 
-    ds = load_dataset_jax(interim_samples_fpath)
+    ds = load_dataset_jax(samples_fpath)
     e1 = ds["samples"]["e1"]
     e2 = ds["samples"]["e2"]
     e1e2 = jnp.stack([e1, e2], axis=-1)
     sigma_e_int = ds["hyper"]["sigma_e_int"]
     _logtarget = jit(partial(logtarget_shear_and_sn, sigma_e_int=sigma_e_int))
+
     rng_key = jax.random.key(seed)
+    k1, k2 = random.split(rng_key)
+
+    # initialize shape noise in small ball around truth
+    _sigma_e_true = ds["hyper"]["shape_noise"]
+    sigma_e_initial = random.uniform(
+        k1, shape=(), minval=_sigma_e_true - 0.02, maxval=_sigma_e_true + 0.02
+    )
+
     samples = run_inference_nuts(
-        rng_key,
+        k2,
         data=e1e2,
-        init_positions={"g": jnp.array([0.0, 0.0]), "sigma_e": 0.2},
+        init_positions={"g": jnp.array([0.0, 0.0]), "sigma_e": sigma_e_initial},
         logtarget=_logtarget,
         n_samples=n_samples,
         initial_step_size=initial_step_size,
