f5c0930f96743033f7625970e1e64b19cc5b4437
ismael2395 <imendoza@umich.edu>
2025-04-10 14:14:02 -0700

    changes to exp 23

--- experiments/exp23/get_samples_full.py
+++ experiments/exp23/get_samples_full.py
@@ -17,7 +17,7 @@ from bpd.prior import interim_gprops_logprior
 from bpd.sample import (
     get_target_images,
     get_true_params_from_galaxy_params,
-    sample_galaxy_params_trunc,
+    sample_galaxy_params_skew,
 )
 
 
@@ -58,9 +58,9 @@ def main(
     fft_size: int = 256,
     background: float = 1.0,
     initial_step_size: float = 0.1,
-    mean_logflux: float = 2.5,
+    a_logflux: float = 14,
+    mean_logflux: float = 2.45,
     sigma_logflux: float = 0.4,
-    min_logflux: float = 2.45,
     mean_loghlr: float = -0.4,
     sigma_loghlr: float = 0.05,
 ):
@@ -87,13 +87,13 @@ def main(
     )
 
     # galaxy parameters from prior
-    galaxy_params = sample_galaxy_params_trunc(
+    galaxy_params = sample_galaxy_params_skew(
         pkey,
         n=n_gals,
         shape_noise=shape_noise,
+        a_logflux=a_logflux,
         mean_logflux=mean_logflux,
         sigma_logflux=sigma_logflux,
-        min_logflux=min_logflux,
         mean_loghlr=mean_loghlr,
         sigma_loghlr=sigma_loghlr,
     )
