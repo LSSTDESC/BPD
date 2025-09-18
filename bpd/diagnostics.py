36cc7afaf6c5e45f8e5ac6d5a576cf71fc0810be
Ismael Mendoza <11745764+ismael-mendoza@users.noreply.github.com>
2025-09-18 13:30:49 -0400

    eta PC experiment (#102)
    * eta draft script
    
    * add sher inference
    
    * posterior calibration intial rseults
    
    * works with distinct truth
    
    * added more diagnostics from paper
    
    * will do cmparison soon
    
    * ignore npy in notebooks
    
    * we choose a sigma_m = 0.2 based on matching sampled distribution
    
    * script to run samples of shear from eta parametrization
    
    * add rank figures that might be used in the future
    
    * add posterior calibration figure with eta
    
    * tests

--- bpd/diagnostics.py
+++ bpd/diagnostics.py
@@ -3,12 +3,13 @@ import pandas as pd
 from chainconsumer import Chain, ChainConsumer, Truth
 from matplotlib.figure import Figure
 from matplotlib.pyplot import Axes
+from numpy import ndarray
 from numpyro.diagnostics import hpdi
 from scipy import stats
 
 
 def get_contour_plot(
-    samples_list: list[dict[str, np.ndarray]],
+    samples_list: list[dict[str, ndarray]],
     names: list[str],
     truth: dict[str, float],
     figsize: tuple[float, float] = (7, 7),
@@ -22,9 +23,7 @@ def get_contour_plot(
     return c.plotter.plot(figsize=figsize)
 
 
-def get_gauss_pc_fig(
-    ax: Axes, samples: np.ndarray, truth: float, param_name: str | None = None
-) -> None:
+def get_gauss_pc_fig(ax: Axes, samples: ndarray, truth: float | ndarray) -> None:
     """Get a marginal pc figure assuming Gaussian distribution of samples."""
     assert samples.ndim == 2  # (n_chains, n_samples)
     cis = np.linspace(0.05, 1, 20)
@@ -36,22 +35,17 @@ def get_gauss_pc_fig(
         counts &= truth > samples.mean(axis=1) - s * samples.std(axis=1)
         fractions.append(counts.mean().item())
     fractions = np.array(fractions)
-    ax.plot(cis, fractions, "-x", color="C0")
+    ax.plot(cis, fractions, "-", color="C0", lw=2)
     tick_labels = [0, 0.25, 0.5, 0.75, 1.0]
     ax.plot(cis, cis, "--k", label="calibrated")
-    ax.legend(loc="best", prop={"size": 14})
-    ax.set_xlabel(r"Target coverage", fontsize=14)
-    ax.set_ylabel(r"Realized coverage", fontsize=14)
     ax.set_xticks(tick_labels)
     ax.set_yticks(tick_labels)
-    ax.set_title(param_name, fontsize=14)
 
 
 def get_pc_fig(
     ax: Axes,
-    samples: np.ndarray,
-    truth: float,
-    param_name: str | None = None,
+    samples: ndarray,
+    truth: float | ndarray,
     n_bins: int = 20,
 ) -> None:
     """Get a marginal probability calibration figure using `hpdi` from `numpyro`."""
@@ -64,12 +58,79 @@ def get_pc_fig(
         counts = (truth > ci1) & (truth < ci2)
         fractions.append(counts.mean().item())
     fractions = np.array(fractions)
-    ax.plot(ci_bins, fractions, "-x", color="C0")
+    ax.plot(ci_bins, fractions, "-", color="C0", lw=2)
     tick_labels = [0, 0.25, 0.5, 0.75, 1.0]
     ax.plot(ci_bins, ci_bins, "--k", label="calibrated")
-    ax.legend(loc="best", prop={"size": 14})
-    ax.set_xlabel(r"Target coverage", fontsize=14)
-    ax.set_ylabel(r"Realized coverage", fontsize=14)
     ax.set_xticks(tick_labels)
     ax.set_yticks(tick_labels)
-    ax.set_title(param_name, fontsize=14)
+
+
+def get_rank_pc_figs(
+    samples: ndarray,
+    truth: ndarray,
+    *,
+    axes,
+    n_bins: int = 21,
+    n_boots: int = 10_000,
+    alpha: float = 0.99,  # confidence interval
+):
+    # we check the uniformity of ranks in various ways
+    assert samples.ndim == 2 and truth.ndim == 1
+    assert samples.shape[0] == truth.shape[0]
+    ranks = np.sum((samples < truth.reshape(-1, 1)), axis=1)
+
+    _, bins = np.histogram(ranks, bins=n_bins)
+
+    # use simulations to get confidence interval for ranks
+    high = samples.shape[-1]
+    boot_ranks = np.random.randint(low=0, high=high, size=(n_boots, ranks.shape[0]))
+
+    # get histogram and cdfs for comparison on same bins
+
+    hists = []
+    ecdfs = []
+    for ii in range(len(boot_ranks)):
+        hist, _ = np.histogram(boot_ranks[ii], bins=bins)
+
+        hist_normed, _ = np.histogram(boot_ranks[ii], bins=bins, density=True)
+        ecdf = np.cumsum(hist_normed)
+        ecdf /= ecdf[-1]
+
+        hists.append(hist)
+        ecdfs.append(ecdf)
+
+    hists = np.stack(hists, axis=0)
+    ecdfs = np.stack(ecdfs, axis=0)
+
+    low = np.quantile(hists, (1 - alpha) / 2, axis=0)
+    med = np.quantile(hists, 0.5, axis=0)
+    high = np.quantile(hists, (1 + alpha) / 2, axis=0)
+
+    low_ecdf = np.quantile(ecdfs, (1 - alpha) / 2, axis=0)
+    median_ecdf = np.quantile(ecdfs, 0.5, axis=0)
+    high_ecdf = np.quantile(ecdfs, (1 + alpha) / 2, axis=0)
+
+    # figure 1
+    ax = axes[0]
+    ax.hist(ranks, bins=bins, histtype="step")
+    ax.axhline(low.mean(), c="k", ls="--")
+    ax.axhline(high.mean(), c="k", ls="--")
+    ax.axhline(med.mean(), c="r", ls="--")
+
+    # figure 2
+    ax = axes[1]
+    mids = (bins[1:] + bins[:-1]) / 2
+    vals, _, _ = ax.hist(
+        ranks, bins=bins, histtype="step", cumulative=True, density=True
+    )
+    ax.step(mids, low_ecdf, where="mid", ls="--")
+    ax.step(mids, high_ecdf, where="mid", ls="--")
+    ax.step(mids, median_ecdf, where="mid", ls="--")
+
+    # figure 3
+    ax = axes[2]
+    ax.plot(mids, vals - median_ecdf)
+    ax.fill_between(
+        mids, low_ecdf - median_ecdf, high_ecdf - median_ecdf, color="k", alpha=0.1
+    )
+    ax.axhline(0.0, c="k", ls="--")
---
+++ experiments/exp81/eta_experiment.py
@@ -0,0 +1,214 @@
+#!/usr/bin/env python3
+from functools import partial
+
+import jax.numpy as jnp
+import jax.scipy as jsp
+import typer
+from jax import Array, grad, jit, random, vmap
+from jax.random import PRNGKey
+from jax.scipy.stats import norm
+
+from bpd import DATA_DIR
+from bpd.chains import run_inference_nuts
+from bpd.io import save_dataset
+from bpd.prior import ellip_prior_e1e2
+from bpd.sample import sample_ellip_prior
+from bpd.shear import (
+    inv_shear_func1,
+    inv_shear_func2,
+    inv_shear_transformation,
+    shear_transformation,
+)
+from bpd.utils import process_in_batches, uniform_logpdf
+
+eta_inv_fnc1 = lambda eta: eta2g(eta)[0]
+eta_inv_fnc2 = lambda eta: eta2g(eta)[1]
+
+grad_fnc1 = grad(eta_inv_fnc1)
+grad_fnc2 = grad(eta_inv_fnc2)
+
+
+def eta2g(eta: Array):
+    assert eta.shape == (2,)
+    eta1 = eta[0]
+    eta2 = eta[1]
+    abseta = jnp.sqrt(eta1**2 + eta2**2)
+    g1 = eta1 * jnp.tanh(0.5 * abseta) / abseta
+    g2 = eta2 * jnp.tanh(0.5 * abseta) / abseta
+    return jnp.array([g1, g2])
+
+
+def g2eta(g: Array):
+    assert g.shape == (2,)
+    g1 = g[0]
+    g2 = g[1]
+    absg = jnp.sqrt(g1**2 + g2**2)
+    eta1 = g1 * jnp.arctanh(absg) * 2 / absg
+    eta2 = g2 * jnp.arctanh(absg) * 2 / absg
+    return jnp.array([eta1, eta2])
+
+
+def sample_noisy_eta(rng_key, *, g, sigma_e: float, sigma_m: float, n: int = 1):
+    k1, k2 = random.split(rng_key)
+    es = sample_ellip_prior(k1, sigma_e, n)
+    essh = shear_transformation(es, g)
+    etas = vmap(g2eta)(essh)
+    noisy_etas = etas.reshape(n, 2) + random.normal(k2, shape=(n, 2)) * sigma_m
+    return noisy_etas, etas
+
+
+def eta_target(eta, *, data, sigma_e: float, sigma_m: float):
+    neta = data
+    llike = norm.logpdf(neta, loc=eta, scale=sigma_m).sum()
+
+    e1e2 = eta2g(eta)
+    lprior1 = jnp.log(ellip_prior_e1e2(e1e2, sigma_e))
+
+    grad1 = grad_fnc1(eta)
+    grad2 = grad_fnc2(eta)
+    prior2 = jnp.abs(grad1[..., 0] * grad2[..., 1] - grad1[..., 1] * grad2[..., 0])
+    lprior2 = jnp.log(prior2)
+
+    return llike + lprior1 + lprior2
+
+
+_grad_shear_fnc1 = vmap(
+    vmap(grad(inv_shear_func1), in_axes=(0, None)), in_axes=(0, None)
+)
+_grad_shear_fnc2 = vmap(
+    vmap(grad(inv_shear_func2), in_axes=(0, None)), in_axes=(0, None)
+)
+
+# already vmapped once
+_inv_shear_trans = vmap(inv_shear_transformation, in_axes=(0, None))
+
+
+def shear_eta_target(g, *, data, sigma_e: float, sigma_e_int: float):
+    assert g.shape == (2,)
+    assert data.ndim == 3 and data.shape[2] == 2
+    etas = data
+
+    # P(eta' | alpha, g) = P(eps | alpha) * (del eps' / del eta') * (del eps / del eps')
+    # jacobian on eta cancels between num and denom so we ignore it.
+    eps_sheared = vmap(vmap(eta2g))(etas)
+    eps = _inv_shear_trans(eps_sheared, g)
+    num1 = jnp.log(ellip_prior_e1e2(eps, sigma_e))
+
+    grad_eps1 = _grad_shear_fnc1(eps_sheared, g)
+    grad_eps2 = _grad_shear_fnc2(eps_sheared, g)
+    jac = jnp.abs(
+        grad_eps1[..., 0] * grad_eps2[..., 1] - grad_eps1[..., 1] * grad_eps2[..., 0]
+    )
+    num2 = jnp.log(jac)
+
+    num = num1 + num2
+
+    # now denom P0(eta') = P0(eps') * (del eps' / del eta')
+    denom = jnp.log(ellip_prior_e1e2(eps_sheared, sigma_e_int))
+
+    # ratio
+    ratio = jsp.special.logsumexp(num - denom, axis=-1)
+    loglike = ratio.sum()
+
+    # prior on shear
+    g_mag = jnp.sqrt(g[0] ** 2 + g[1] ** 2)
+    logprior = uniform_logpdf(g_mag, 0.0, 1.0) + jnp.log(1 / (2 * jnp.pi))
+
+    return logprior + loglike
+
+
+def run_one_eta_experiment(
+    rng_key: PRNGKey,
+    true_g,
+    *,
+    n_gals: int,
+    n_samples_per_gal: int,
+    n_samples_shear: int,
+    sigma_e: float,
+    sigma_e_int: float,
+    sigma_m: float,
+):
+    k1, k2, k3 = random.split(rng_key, 3)
+
+    # generate data
+    netas, etas = sample_noisy_eta(
+        k1, g=true_g, sigma_e=sigma_e, sigma_m=sigma_m, n=n_gals
+    )
+
+    # run phase 1
+    target1 = partial(eta_target, sigma_e=sigma_e_int, sigma_m=sigma_m)
+    pipe1 = partial(
+        run_inference_nuts,
+        logtarget=target1,
+        n_samples=n_samples_per_gal,
+        initial_step_size=0.01,
+        max_num_doublings=2,
+    )
+    pipe1 = vmap(jit(pipe1))
+    k2s = random.split(k2, n_gals)
+    eta_samples = pipe1(k2s, netas, etas)
+
+    # run phase 2
+    target2 = partial(shear_eta_target, sigma_e=sigma_e, sigma_e_int=sigma_e_int)
+    pipe2 = partial(
+        run_inference_nuts,
+        logtarget=target2,
+        n_samples=n_samples_shear,
+        initial_step_size=0.01,
+        max_num_doublings=2,
+    )
+    pipe2 = jit(pipe2)
+
+    g_samples = pipe2(k3, eta_samples, true_g)
+    return g_samples
+
+
+# draw shear samples from prior (unit disk)
+def sample_shear_prior(rng_key):
+    k1, k2 = random.split(rng_key)
+    g_mag = random.uniform(k1, minval=-0.0, maxval=1.0)
+    theta = random.uniform(k2, minval=0.0, maxval=jnp.pi)
+    g1 = g_mag * jnp.cos(2 * theta)
+    g2 = g_mag * jnp.sin(2 * theta)
+    return jnp.array([g1, g2])
+
+
+def main(seed: int = 52, n_exps: int = 1000):
+    save_dir = DATA_DIR / "cache_chains" / f"exp81_{seed}"
+    save_dir.mkdir(exist_ok=True)
+
+    k = random.key(seed)
+    k1, k2 = random.split(k)
+    k1s = random.split(k1, n_exps)
+    k2s = random.split(k2, n_exps)
+
+    pipe = partial(
+        run_one_eta_experiment,
+        n_gals=1000,
+        n_samples_per_gal=300,
+        n_samples_shear=1024 - 1,
+        sigma_e=0.2,
+        sigma_e_int=0.3,
+        sigma_m=0.2,
+    )
+    pipe = jit(pipe)
+
+    # run once (jitting)
+    _ = pipe(k1s[0], jnp.array([0.02, 0.0]))
+
+    true_gs = vmap(sample_shear_prior)(k1s)
+    g_samples = process_in_batches(
+        vmap(pipe),
+        k2s,
+        true_gs,
+        n_points=n_exps,
+        batch_size=100,
+    )
+
+    save_dataset(
+        {"g_samples": g_samples, "true_g": true_gs}, save_dir / "eta_shear_samples.npz"
+    )
+
+
+if __name__ == "__main__":
+    typer.run(main)
