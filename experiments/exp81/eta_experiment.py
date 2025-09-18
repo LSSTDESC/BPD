c28db987437bc2c794c9660ffc88950ce012978e
ismael2395 <imendoza@umich.edu>
2025-09-18 09:52:37 -0700

    script to run samples of shear from eta parametrization

--- experiments/exp81/eta_experiment.py
+++ experiments/exp81/eta_experiment.py
@@ -3,11 +3,14 @@ from functools import partial
 
 import jax.numpy as jnp
 import jax.scipy as jsp
+import typer
 from jax import Array, grad, jit, random, vmap
 from jax.random import PRNGKey
 from jax.scipy.stats import norm
 
+from bpd import DATA_DIR
 from bpd.chains import run_inference_nuts
+from bpd.io import save_dataset
 from bpd.prior import ellip_prior_e1e2
 from bpd.sample import sample_ellip_prior
 from bpd.shear import (
@@ -16,7 +19,7 @@ from bpd.shear import (
     inv_shear_transformation,
     shear_transformation,
 )
-from bpd.utils import uniform_logpdf
+from bpd.utils import process_in_batches, uniform_logpdf
 
 eta_inv_fnc1 = lambda eta: eta2g(eta)[0]
 eta_inv_fnc2 = lambda eta: eta2g(eta)[1]
@@ -102,6 +105,8 @@ def shear_eta_target(g, *, data, sigma_e: float, sigma_e_int: float):
 
     # now denom P0(eta') = P0(eps') * (del eps' / del eta')
     denom = jnp.log(ellip_prior_e1e2(eps_sheared, sigma_e_int))
+
+    # ratio
     ratio = jsp.special.logsumexp(num - denom, axis=-1)
     loglike = ratio.sum()
 
@@ -114,7 +119,7 @@ def shear_eta_target(g, *, data, sigma_e: float, sigma_e_int: float):
 
 def run_one_eta_experiment(
     rng_key: PRNGKey,
-    g1: float,
+    true_g,
     *,
     n_gals: int,
     n_samples_per_gal: int,
@@ -127,7 +132,7 @@ def run_one_eta_experiment(
 
     # generate data
     netas, etas = sample_noisy_eta(
-        k1, g=jnp.array([g1, 0.0]), sigma_e=sigma_e, sigma_m=sigma_m, n=n_gals
+        k1, g=true_g, sigma_e=sigma_e, sigma_m=sigma_m, n=n_gals
     )
 
     # run phase 1
@@ -154,5 +159,56 @@ def run_one_eta_experiment(
     )
     pipe2 = jit(pipe2)
 
-    g_samples = pipe2(k3, eta_samples, jnp.array([g1, 0.0]))
+    g_samples = pipe2(k3, eta_samples, true_g)
     return g_samples
+
+
+# draw shear samples from prior (unit disk)
+def sample_shear_prior(rng_key):
+    k1, k2 = random.split(rng_key)
+    g_mag = random.uniform(k1, minval=-0.0, maxval=1.0)
+    theta = random.uniform(k2, minval=0.0, maxval=jnp.pi)
+    g1 = g_mag * jnp.cos(2 * theta)
+    g2 = g_mag * jnp.sin(2 * theta)
+    return jnp.array([g1, g2])
+
+
+def main(seed: int = 52, n_exps: int = 1000):
+    save_dir = DATA_DIR / "cache_chains" / f"exp81_{seed}"
+    save_dir.mkdir(exist_ok=True)
+
+    k = random.key(seed)
+    k1, k2 = random.split(k)
+    k1s = random.split(k1, n_exps)
+    k2s = random.split(k2, n_exps)
+
+    pipe = partial(
+        run_one_eta_experiment,
+        n_gals=1000,
+        n_samples_per_gal=300,
+        n_samples_shear=1024 - 1,
+        sigma_e=0.2,
+        sigma_e_int=0.3,
+        sigma_m=0.2,
+    )
+    pipe = jit(pipe)
+
+    # run once (jitting)
+    _ = pipe(k1s[0], jnp.array([0.02, 0.0]))
+
+    true_gs = vmap(sample_shear_prior)(k1s)
+    g_samples = process_in_batches(
+        vmap(pipe),
+        k2s,
+        true_gs,
+        n_points=n_exps,
+        batch_size=100,
+    )
+
+    save_dataset(
+        {"g_samples": g_samples, "true_g": true_gs}, save_dir / "eta_shear_samples.npz"
+    )
+
+
+if __name__ == "__main__":
+    typer.run(main)
