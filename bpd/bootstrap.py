ed40669fbe68ecdae008ac81bda712f9edb4b50b
ismael2395 <imendoza@umich.edu>
2025-09-24 11:42:20 -0700

    split into two

--- bpd/bootstrap.py
+++ bpd/bootstrap.py
@@ -1,8 +1,10 @@
 from typing import Callable
 
 import jax.numpy as jnp
-from jax import device_put, jit, random
+from jax import device_put, jit, random, vmap
 from tqdm import tqdm
+
+from bpd.utils import process_in_batches
 
 
 def run_bootstrap(
@@ -40,3 +42,46 @@ def run_bootstrap(
         samples[k] = jnp.stack([rs[k] for rs in results], axis=0)
 
     return samples
+
+
+def run_bootstrap_shear_vectorized(
+    rng_key,
+    *args,  # arguments to vectorize like "init_positions".
+    post_params_plus: dict,
+    post_params_minus: dict,
+    shear_pipeline: Callable,
+    n_gals: int,
+    n_boots: int = 100,
+    n_splits: int = 10,
+    no_bar: bool = True,
+):
+    for p in post_params_plus:
+        assert n_gals == post_params_plus[p].shape[0]
+
+    k1, k2 = random.split(rng_key)
+    k2s = random.split(k2, n_boots)
+    indices = random.randint(k1, shape=(n_boots, n_gals), minval=0, maxval=n_gals)
+    boot_ppp = {k: v[indices] for k, v in post_params_plus.items()}
+    boot_ppm = {k: v[indices] for k, v in post_params_minus.items()}
+
+    batch_size = n_boots // n_splits
+    samples_plus = process_in_batches(
+        vmap(shear_pipeline),
+        k2s,
+        boot_ppp,
+        *args,
+        n_points=n_boots,
+        batch_size=batch_size,
+        no_bar=no_bar,
+    )
+    samples_minus = process_in_batches(
+        vmap(shear_pipeline),
+        k2s,
+        boot_ppm,
+        *args,
+        n_points=n_boots,
+        batch_size=batch_size,
+        no_bar=no_bar,
+    )
+
+    return samples_plus, samples_minus
---
+++ bpd/jackknife.py
@@ -0,0 +1,140 @@
+from math import ceil
+from typing import Callable
+
+import jax.numpy as jnp
+from jax import jit, random, vmap
+from tqdm import tqdm
+
+
+def run_jackknife_shear_pipeline(
+    rng_key,
+    *,
+    post_params_plus: dict,
+    post_params_minus: dict,
+    shear_pipeline: Callable,
+    n_gals: int,
+    n_jacks: int = 100,
+    start: int = 0,
+    end: int | None = None,
+    no_bar: bool = True,
+):
+    """Use jackknife+shape noise cancellation to estimate the mean and std of the shear posterior.
+
+    Args:
+        rng_key: Random jax key.
+        init_g: Initial value for shear `g`.
+        post_params_pos: Interim posterior galaxy parameters estimated using positive shear.
+        post_params_neg: Interim posterior galaxy parameters estimated using negative shear,
+            and otherwise same conditions and random seed as `post_params_pos`.
+        shear_pipeline: Function that outputs shear posterior samples from `post_params` with all
+            keyword arguments pre-specified.
+        n_jacks: Number of jackknife batches.
+
+    Returns:
+        Jackknife samples of shear posterior mean combined with shape noise cancellation trick.
+    """
+    if end is None:
+        end = n_jacks
+
+    batch_size = ceil(n_gals / n_jacks)
+
+    results_plus = []
+    results_minus = []
+    keys = random.split(rng_key, n_jacks)
+
+    pipe = jit(shear_pipeline)
+
+    for ii in tqdm(range(start, end), desc="Jackknife #", disable=no_bar):
+        k_ii = keys[ii]
+        idx1, idx2 = ii * batch_size, (ii + 1) * batch_size
+
+        _params_jack_pos = {
+            k: jnp.concatenate([v[:idx1], v[idx2:]])
+            for k, v in post_params_plus.items()
+        }
+        _params_jack_neg = {
+            k: jnp.concatenate([v[:idx1], v[idx2:]])
+            for k, v in post_params_minus.items()
+        }
+
+        g_pos_ii = pipe(k_ii, _params_jack_pos)
+        g_neg_ii = pipe(k_ii, _params_jack_neg)
+
+        results_plus.append(g_pos_ii)
+        results_minus.append(g_neg_ii)
+
+    g_pos_samples = jnp.stack(results_plus, axis=0)
+    g_neg_samples = jnp.stack(results_minus, axis=0)
+    assert g_pos_samples.shape[-1] == 2
+    assert g_neg_samples.shape[-1] == 2
+
+    return g_pos_samples, g_neg_samples
+
+
+def run_jackknife_vectorized(
+    rng_key,
+    *,
+    post_params_plus: dict,
+    post_params_minus: dict,
+    shear_pipeline: Callable,
+    n_gals: int,
+    n_jacks: int = 100,
+    n_splits: int = 2,
+    no_bar: bool = False,
+):
+    """Same as previous function, but vectorized for speed."""
+    b1 = int(n_jacks / n_splits)
+    b2 = int(n_gals / n_jacks)
+    assert n_jacks % n_splits == 0
+    assert n_gals % n_jacks == 0, "# of galaxies needs to be divisible by # jackknives."
+
+    keys = random.split(rng_key, n_jacks)
+
+    vec_shear_pipeline = jit(vmap(shear_pipeline, in_axes=(0, 0, None)))
+
+    results_plus = []
+    results_minus = []
+
+    for ii in tqdm(range(n_splits), disable=no_bar, desc="Splits JK"):
+        start1, end1 = ii * b1, (ii + 1) * b1
+
+        # prepare dictionaries of jackknife samples
+        params_jack_pos = {}
+        params_jack_neg = {}
+        for k in post_params_plus:
+            v1 = post_params_plus[k]
+            v2 = post_params_minus[k]
+            all_jack_params_pos = []
+            all_jack_params_neg = []
+            for jj in range(start1, end1):
+                start2, end2 = jj * b2, (jj + 1) * b2
+                all_jack_params_pos.append(jnp.concatenate([v1[:start2], v1[end2:]]))
+                all_jack_params_neg.append(jnp.concatenate([v2[:start2], v2[end2:]]))
+
+            params_jack_pos[k] = jnp.stack(all_jack_params_pos, axis=0)
+            params_jack_neg[k] = jnp.stack(all_jack_params_neg, axis=0)
+
+            assert params_jack_pos[k].shape[0] == b1
+
+        if ii == 0:
+            # run first a single example for compilation purposes
+            _ = vec_shear_pipeline(
+                keys[0, None],
+                {k: v[0, None] for k, v in params_jack_pos.items()},
+            )
+
+        gp = vec_shear_pipeline(keys[start1:end1], params_jack_pos)
+        gn = vec_shear_pipeline(keys[start1:end1], params_jack_neg)
+
+        results_plus.append(gp)
+        results_minus.append(gn)
+
+    g_pos_samples = jnp.concatenate(results_plus)
+    g_neg_samples = jnp.concatenate(results_minus)
+
+    assert g_pos_samples.shape[0] == n_jacks
+    assert g_pos_samples.shape[-1] == 2
+    assert g_neg_samples.shape[0] == n_jacks
+    assert g_neg_samples.shape[-1] == 2
+
+    return g_pos_samples, g_neg_samples
