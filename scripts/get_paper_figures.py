b1621f8a1942bdb00f87706d4f32c30ae5442e6c
ismael2395 <imendoza@umich.edu>
2026-02-13 12:17:51 -0800

    add timing table

--- scripts/get_paper_figures.py
+++ scripts/get_paper_figures.py
@@ -24,6 +24,7 @@ from bpd.draw import draw_exponential_galsim
 from bpd.io import load_dataset, save_dataset
 from bpd.plotting import (
     get_timing_figure,
+    get_timing_table,
     set_rc_params,
 )
 from bpd.sample import sample_galaxy_params_skew
@@ -126,6 +127,7 @@ OUT_PATHS = {
     "boot_bias": FIG_DIR / "table_bias_boot.txt",
     "eta_pc": FIG_DIR / "eta_pc.png",
     "model_bias": FIG_DIR / "model_bias.png",
+    "timing_table": FIG_DIR / "timing_table.txt",
 }
 
 _nu_hash = {
@@ -197,7 +199,7 @@ def make_distribution_figure(fpath: str | Path, overwrite: bool = False):
         density=True,
         range=(0, 4000),
     )
-    ax1.set_xlabel(r"\rm Flux (counts)")
+    ax1.set_xlabel(r"\rm Flux")
     ax1.axvline(
         np.median(params["f"]),
         linestyle="--",
@@ -298,6 +300,22 @@ def make_timing_figure(fpath1: Path, fpath2: Path):
     fig2.savefig(fpath2, format="png")
     plt.close(fig1)
     plt.close(fig2)
+
+
+def make_timing_table(fpath: Path):
+    print("INFO: Making timing table")
+    timing_results = load_dataset(INPUT_PATHS["timing_results"])
+    conv_results = load_dataset(INPUT_PATHS["timing_conv"])
+    ess_dict = conv_results["ess"]
+
+    max_n_gal = str(max(int(k) for k in timing_results))
+    avg_ess = np.mean([np.mean(ess_dict[k]) for k in ess_dict])
+
+    print(f"Avg. ESS: {avg_ess:.3g}")
+
+    get_timing_table(
+        results=timing_results, max_n_gal_str=max_n_gal, avg_ess=avg_ess, fpath=fpath
+    )
 
 
 def make_contour_shear_figure(fpath: str | Path):
@@ -780,8 +798,9 @@ def make_model_bias_figure(fpath: str | Path):
 
 
 def main(overwrite: bool = False):
-    make_distribution_figure(OUT_PATHS["galaxy_distributions"], overwrite=overwrite)
+    # make_distribution_figure(OUT_PATHS["galaxy_distributions"], overwrite=overwrite)
     # make_timing_figure(OUT_PATHS["timing"], OUT_PATHS["timing2"])
+    make_timing_table(OUT_PATHS["timing_table"])
     # make_contour_shear_figure(OUT_PATHS["contour_shear"])
     # make_contour_hyper_figure(OUT_PATHS["contour_hyper"])
     # get_bias_table_subset(OUT_PATHS["subset_bias"])
